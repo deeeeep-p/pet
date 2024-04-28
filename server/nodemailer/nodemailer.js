5d9027052027ca7834caeca40260dc3920c0bc46
Deep <deeppatel223204@gmail.com>
2024-04-28 09:29:08 +0530

    nlswdq

--- server/nodemailer/nodemailer.js
+++ server/nodemailer/nodemailer.js
@@ -1,6 +1,6 @@
 const nodemailer = require("nodemailer");
 
-const sendMail = async () => {
+const sendMail = async (req, res) => {
   let testAccount = await nodemailer.createTestAccount();
 
   // connect with the smtp
@@ -12,17 +12,17 @@ const sendMail = async () => {
       pass: "QJtBYSm2xPAFJ9NWv5",
     },
   });
-
+  const lat = "19.1258452";
+  const long = "72.8166552";
   let info = await transporter.sendMail({
     from: '"Vinod Thapa 👻" <thapa@gmail.com>', // sender address
     to: "iam@gmail.com", // list of receivers
-    subject: "Hello Thapa", // Subject line
-    text: "Hello YT Thapa", // plain text body
+    subject: "location of the pet", // Subject line
+    text: `your cat was found in this location at last: https://www.google.com/maps/@${lat},${long}11z?entry=ttu`, // plain text body
   });
 
   console.log("Message sent: %s", info.messageId);
   // res.json(info);
 };
-sendMail();
 
-// module.exports = sendMail;
+module.exports = { sendMail };
