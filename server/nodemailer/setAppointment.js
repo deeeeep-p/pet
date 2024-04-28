46b684863b5f9bc5a9771226e7cf80c052832fae
Deep <deeppatel223204@gmail.com>
2024-04-28 16:55:03 +0530

    last commit lfg🔥

--- server/nodemailer/setAppointment.js
+++ server/nodemailer/setAppointment.js
@@ -1,27 +1,33 @@
 const nodemailer = require("nodemailer");
 
 const sendMail = async (req, res) => {
-  let testAccount = await nodemailer.createTestAccount();
+  try {
+    let testAccount = await nodemailer.createTestAccount();
 
-  // connect with the smtp
-  let transporter = await nodemailer.createTransport({
-    host: "smtp.ethereal.email",
-    port: 587,
-    auth: {
-      user: "yvette26@ethereal.email",
-      pass: "QJtBYSm2xPAFJ9NWv5",
-    },
-  });
-  let info = await transporter.sendMail({
-    from: '"Vinod Thapa " <thapa@gmail.com>', // sender address
-    to: "Vineet@fortis.com", // list of receivers
-    subject: "Reminder about the Apponitment ", // Subject line
-    text: `Your appointmenr has been setup for 22-04-24, at 5pm est`, // plain text body
-  });
+    // Connect with the SMTP server
+    let transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      auth: {
+        user: "yvette26@ethereal.email",
+        pass: "QJtBYSm2xPAFJ9NWv5",
+      },
+    });
 
-  console.log("Message sent: %s", info.messageId);
-  res.send("Message sent: %s", info.messageId);
-  // res.json(info);
+    // Compose the email message
+    let info = await transporter.sendMail({
+      from: '"Vinod Thapa " <thapa@gmail.com>', // sender address
+      to: "Vinod@fortis.com", // list of receivers
+      subject: "Reminder about the Appointment", // Subject line
+      text: `Your appointment has been setup for 22-04-24, at 5pm IST`, // Plain text body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    res.status(200).send(`Message sent: ${info.messageId}`);
+  } catch (error) {
+    console.error("Error occurred while sending email:", error);
+    res.status(500).send("Error occurred while sending email");
+  }
 };
 
 module.exports = { sendMail };
