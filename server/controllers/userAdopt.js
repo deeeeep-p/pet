c2162e3c5d7ed8a526ad905b147e68d98ab18c05
drishya <drishyaveena21@gmail.com>
2024-04-28 08:35:50 +0530

    adopter_setup

--- server/controllers/userAdopt.js
+++ server/controllers/userAdopt.js
@@ -1,5 +1,6 @@
 const UserAdopt = require("../models/userAdopt");
 const createUserAdopt = async (req, res) => {
+  console.log(req.body.loca);
   try {
     const userAdopt = await UserAdopt.create(req.body);
     return res.status(200).json({ userAdopt });
